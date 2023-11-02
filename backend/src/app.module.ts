6554e91eb392c165eaaef4bade57bae4c60f0887
dev <cunhafelipe098@gmail.com>
2023-11-02 13:36:39 -0300

    chore: prisma module configuration and connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FavoriteModule } from './favorite/favorite.module';
 import { MovieModule } from './movie/movie.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FavoriteModule, MovieModule],
+  imports: [FavoriteModule, MovieModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- backend/src/favorite/favorite.service.ts
+++ backend/src/favorite/favorite.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { Favorite, Movie } from '@prisma/client';
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class FavoriteService {
+    constructor(private prisma: PrismaService) {}
+
     markAsFavorite() {
         return {msg: 'Hello World'}
     };
