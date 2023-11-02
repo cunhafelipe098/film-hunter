f2f7c7783b154773e87dcd7f7d384d591de21589
dev <cunhafelipe098@gmail.com>
2023-11-02 19:36:56 -0300

    refactor: refactor to start use nestjs config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { FavoriteModule } from './favorite/favorite.module';
 import { MovieModule } from './movie/movie.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FavoriteModule, MovieModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    FavoriteModule, 
+    MovieModule, 
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
