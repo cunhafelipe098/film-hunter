a94221eb0c665f2807f6357c9a0658740da24820
dev <cunhafelipe098@gmail.com>
2023-11-02 18:56:13 -0300

    feat: error handling implementation

--- backend/src/favorite/favorite.service.ts
+++ backend/src/favorite/favorite.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { Favorite, Movie } from '@prisma/client';
 import { PrismaService } from "src/prisma/prisma.service";
 import { MovieDto } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class FavoriteService {
@@ -9,15 +10,26 @@ export class FavoriteService {
 
     async markAsFavorite(dto: MovieDto) {
 
-        const movie = await this.prisma.movie.create({
-            data: { ...dto }
-        });
-        const favorite = await this.prisma.favorite.create({
-            data:{
-                 movieId: movie.id   
+        try {
+            const movie = await this.prisma.movie.create({
+                data: { ...dto }
+            });
+            const favorite = await this.prisma.favorite.create({
+                data:{
+                     movieId: movie.id   
+                }
+            });
+            return {movie, favorite};
+        } catch (error) {
+            if( error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Credentials taken',
+                    );
+                }
+                throw error;
             }
-        });
-        return {movie, favorite};
+        }
     };
 
     unmarkAsFavorite() {
