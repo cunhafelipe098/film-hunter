434799a4e5a58a41b065dcb5a43d7f89d2763530
dev <cunhafelipe098@gmail.com>
2023-11-04 20:25:46 -0300

    feat/frontEnd: creation of the basic structure of a skeleton

---
+++ frontend/src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,14 @@
+import './Skeleton.scss'
+import { Card } from '@ui5/webcomponents-react';
+
+function Skeleton() {
+
+  return (
+    <div >
+      <Card className="skeleton-placeholder">        
+      </Card>
+    </div>
+  )
+}
+
+export default Skeleton
