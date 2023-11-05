2f6b375649400b99b6ace9b5a7056a0e8bfc96a1
dev <cunhafelipe098@gmail.com>
2023-11-05 16:53:39 -0300

    refactor/backEnd: allow cons by frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3333);
 }
 bootstrap();
