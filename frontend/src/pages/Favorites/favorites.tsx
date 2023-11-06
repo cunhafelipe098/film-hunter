a9399610156f9f8c86b5e40977ce9ad76a30cdc4
dev <cunhafelipe098@gmail.com>
2023-11-06 15:19:20 -0300

    feat/frontEnd: develop favorite page

--- frontend/src/pages/Favorites/favorites.tsx
+++ frontend/src/pages/Favorites/favorites.tsx
@@ -1,24 +1,51 @@
 
-import FavoriteSkeleton from './components/favoriteSkeleton';
+import { useEffect, useState } from 'react';
+
+import { api } from '../../components/Api'
 import { 
   Title
 } from '@ui5/webcomponents-react';
 import '@ui5/webcomponents/dist/features/InputSuggestions.js';
 
+import FavoriteSkeleton from './components/favoriteSkeleton';
+import CardMovie, { MovieType } from './components/cardMovie';
+
 import './favorite.scss'
 
 function Favorite() {
+  const [loading, setLoading] = useState(false);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+  useEffect(() => setLoading(false), [favoriteMovies]);
+
+  useEffect(() => {
+    setLoading(true);
+      //setTimeout just to be able to understand skeleton/placehold loading
+      setTimeout(async () => {
+        const response = await api.get(`/movie`);
+        console.log(response);
+        setFavoriteMovies(response.data);
+      }, 3000);
+  }, []);
 
   return (
     <>
       <Title>
-        List of Favorite Movies
+        List of Favorite
       </Title>
 
-      <FavoriteSkeleton/>
+      <div className='favorite-page-container'>
+      {
+        loading ? Array.from({ length: 6 }, () => (
+          <FavoriteSkeleton/>
+        )) : favoriteMovies.map((movie: MovieType) => {
+              return <CardMovie key={movie.id} {...movie}/>
+          })
+      }
+      </div>
     </>
   )
-  }
+}
   
-  export default Favorite
+export default Favorite
   
