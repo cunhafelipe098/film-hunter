0c2431f97248c93d499659a9f192b91b9a6fbee4
dev <cunhafelipe098@gmail.com>
2023-11-06 15:18:34 -0300

    refactor/frontEnd: make favoriteSkeleton render dynamic

--- frontend/src/pages/Favorites/components/favoriteSkeleton.tsx
+++ frontend/src/pages/Favorites/components/favoriteSkeleton.tsx
@@ -1,51 +1,12 @@
 import Skeleton from '../../../components/Skeleton';
-import './favoriteSkeleton.scss';
 
 function FavoriteSkeleton() {
 
     return (
-        <div className='favorite-skeleton-container'>
-            <div>
-                <Skeleton height={25} width={15} busyIndicator={true}/>
-                <Skeleton height={1} width={10} />
-                <Skeleton height={2} width={15} />
-                
-            </div>
-            <div>
-                <Skeleton height={25} width={15} busyIndicator={true}/>
-                <Skeleton height={1} width={10} />
-                <Skeleton height={2} width={15} />
-                
-            </div>
-            <div>
-                <Skeleton height={25} width={15} busyIndicator={true}/>
-                <Skeleton height={1} width={10} />
-                <Skeleton height={2} width={15} />
-            </div>
-
-
-            <div>
-                <Skeleton height={25} width={15} busyIndicator={true}/>
-                <Skeleton height={1} width={10} />
-                <Skeleton height={2} width={15} />
-                
-            </div>
-
-
-            <div>
-                <Skeleton height={25} width={15} busyIndicator={true}/>
-                <Skeleton height={1} width={10} />
-                <Skeleton height={2} width={15} />
-                
-            </div>
-
-
-            <div>
-                <Skeleton height={25} width={15} busyIndicator={true}/>
-                <Skeleton height={1} width={10} />
-                <Skeleton height={2} width={15} />
-                
-            </div>
+        <div>
+            <Skeleton height={25} width={15} busyIndicator={true}/>
+            <Skeleton height={1} width={10} />
+            <Skeleton height={2} width={15} />   
         </div>
     )
   }
